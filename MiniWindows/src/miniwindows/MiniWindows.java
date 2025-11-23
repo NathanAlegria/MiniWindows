3ae2d9e8d556fe66e783debe160f2fdad5d11ef3
Nathan Alegria <nathanalegria02@gmail.com>
2025-11-22 23:00:10 -0600

    creacion de proyecto

---
+++ MiniWindows/src/miniwindows/MiniWindows.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniwindows;
+
+/**
+ *
+ * @author Nathan
+ */
+public class MiniWindows {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
