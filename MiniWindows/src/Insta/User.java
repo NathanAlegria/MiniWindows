6fb9f3c00bf48d96df0e4cbc423becc9ec0dfb66
Nathan Alegria <nathanalegria02@gmail.com>
2025-12-08 06:49:45 -0600

    fingmot

--- MiniWindows/src/Insta/User.java
+++ MiniWindows/src/Insta/User.java
@@ -22,6 +22,7 @@ import java.util.concurrent.TimeUnit;
  * @author jerem
  */
 class User implements Serializable {
+
     private static final long serialVersionUID = 1L;
     private String nombre;
     private char genero;
@@ -48,30 +49,72 @@ class User implements Serializable {
     }
 
     // Getters y Setters
-    public String getNombre() { return nombre; }
-    public char getGenero() { return genero; }
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public int getEdad() { return edad; }
-    public String getFotoPath() { return fotoPath; }
-    public LocalDate getJoinDate() { return joinDate; }
-    public List<Post> getPosts() { return posts; }
-    public List<String> getFollowers() { return followers; }
-    public List<String> getFollowings() { return followings; }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getFotoPath() {
+        return fotoPath;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public List<String> getFollowers() {
+        return followers;
+    }
+
+    public List<String> getFollowings() {
+        return followings;
+    }
 
     // Métodos de acción
-    public void addPost(Post post) { this.posts.add(0, post); } // Añadir al inicio de la lista
-    public boolean isFollowing(String targetUsername) { return followings.contains(targetUsername); }
+    public void addPost(Post post) {
+        this.posts.add(0, post);
+    } // Añadir al inicio de la lista
+
+    public boolean isFollowing(String targetUsername) {
+        return followings.contains(targetUsername);
+    }
+
     public void follow(String targetUsername) {
         if (!isFollowing(targetUsername)) {
             followings.add(targetUsername);
         }
     }
-    public void unfollow(String targetUsername) { followings.remove(targetUsername); }
+
+    public void unfollow(String targetUsername) {
+        followings.remove(targetUsername);
+    }
+
     public void addFollower(String followerUsername) {
         if (!followers.contains(followerUsername)) {
             followers.add(followerUsername);
         }
     }
-    public void removeFollower(String followerUsername) { followers.remove(followerUsername); }
+
+    public void removeFollower(String followerUsername) {
+        followers.remove(followerUsername);
+    }
 }
